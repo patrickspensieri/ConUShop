414a25e5fd723d2609911f81538acd569f4073b3
Karlo Gonzales <karlo.gonzales@gmail.com>
2017-09-19 14:08:46 -0400

    node/heroku init

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.render('public/index.html');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
