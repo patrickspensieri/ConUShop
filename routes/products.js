b46a5129daf65bc61a57a2c25d5969ef9d8ab7a7
Karlo Gonzales <karlo.gonzales@gmail.com>
2017-09-27 14:57:50 -0400

    get all / one for all electronics

--- routes/products.js
+++ routes/products.js
@@ -27,6 +27,110 @@ router.get('/television/:id', (req, res, next) =>{
     });
 });
 
+// retrieves all desktop database entries
+router.get('/desktop', (req, res, next) => {
+    db.query('SELECT * FROM desktop', [], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves one desktop by id
+router.get('/desktop/:id', (req, res, next) =>{
+    let id = parseInt(req.params.id);
+    db.query(`SELECT * FROM desktop WHERE model=${id}`, [], (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves all monitor database entries
+router.get('/monitor', (req, res, next) => {
+    db.query('SELECT * FROM monitor', [], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves one monitor by id
+router.get('/monitor/:id', (req, res, next) =>{
+    let id = parseInt(req.params.id);
+    db.query(`SELECT * FROM monitor WHERE model=${id}`, [], (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves all television database entries
+router.get('/television', (req, res, next) => {
+    db.query('SELECT * FROM television', [], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves one laptop by id
+router.get('/laptop/:id', (req, res, next) =>{
+    let id = parseInt(req.params.id);
+    db.query(`SELECT * FROM laptop WHERE model=${id}`, [], (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves all laptop database entries
+router.get('/laptop', (req, res, next) => {
+    db.query('SELECT * FROM laptop', [], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves one television by id
+router.get('/television/:id', (req, res, next) =>{
+    let id = parseInt(req.params.id);
+    db.query(`SELECT * FROM television WHERE model=${id}`, [], (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves all tablet database entries
+router.get('/tablet', (req, res, next) => {
+    db.query('SELECT * FROM tablet', [], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+// retrieves one tablet by id
+router.get('/tablet/:id', (req, res, next) =>{
+    let id = parseInt(req.params.id);
+    db.query(`SELECT * FROM tablet WHERE model=${id}`, [], (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
 
 //Create row in television table
 /*
