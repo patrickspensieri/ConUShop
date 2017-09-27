344f9dea8021e2d15e44dd395d770cc02311be6f
Karlo Gonzales <karlo.gonzales@gmail.com>
2017-09-27 12:52:21 -0400

    get all / one television

--- routes/products.js
+++ routes/products.js
@@ -6,13 +6,42 @@ let router = new express.Router();
 // require database adapter
 let db = require('../db');
 
-// serves as an example /products/monitors
-router.get('/monitors', (req, res, next) => {
-    db.query('SELECT * FROM monitors', [], (err, res) => {
+// retrieves all television database entries
+router.get('/television', (req, res, next) => {
+    db.query('SELECT * FROM television', [], (err, data) => {
         if (err) {
             return next(err);
         }
-        return next(JSON.stringify(res.rows));
+        res.json(data.rows);
     });
 });
+
+// retrieves one television by id
+router.get('/television/:id', (req, res, next) =>{
+    let id = parseInt(req.params.id);
+    db.query(`SELECT * FROM television WHERE model=${id}`, [], (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+        res.json(data.rows);
+    });
+});
+
+
+//Create row in television table
+/*
+router.post('/television', (req, res, next) => {
+    let brand;
+    let dimensions;
+    let weight;
+    let price;
+    db.query(`insert into television(brand,dimensions,weight,price) values(${brand}, ${dimensions}, ${weight}, ${price})`, [], (err) =>{
+        if (err) {
+            return next(err);
+        }
+        console.log('insert worked')
+    });
+});
+*/
+
 module.exports = router;
