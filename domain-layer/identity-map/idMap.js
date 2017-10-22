7dcf7fca6aa6e04d9b26df0cbe9d730fbe78bb68
CodeTaj <tajbidhussain@hotmail.com>
2017-10-21 23:10:25 -0400

    Cleaning and Commenting

--- domain-layer/identity-map/idMap.js
+++ domain-layer/identity-map/idMap.js
@@ -1,64 +1,96 @@
+let DesktopMapper = require('../mappers/desktopMapper');
+let LaptopMapper = require('../mappers/laptopMapper');
+let MonitorMapper = require('../mappers/monitorMapper');
+let TabletMapper = require('../mappers/tabletMapper');
 
- //null object; will list objects as properties can add properties with []
+/**
+ * Identitymap
+ * @class IdentityMap
+ * @export
+ */
+ class IdentityMap {
 
-//ASSUMING WE ARE MAPPING PER MODEL
+    /**
+     * @constructor
+     * */
+    constructor () {
+         this._identityMap = {};
+    }
 
-class IdentityMap {
-    
-    
-    identityMap = {};
-
-    add(className,object)
-    {   
-        if (object){
-            let mappedObject; // null object
-            if (identityMap[className]){
+    /**
+     * Holds object into the identity map.
+     * To be run when an instance of that object is called from the DB.
+     * @method add
+     * @param {Object} object an object of type electronicDevice
+     */
+    add(object) {   
+        if (object) {
+            var className = object.constructor.name;
+            let mappedObject;
+            if (identityMap[className]) {
                 mappedObject = identityMap[className][object.model];
                 if (mappedObject) {
-                    console.log("this object is already mapped");
+                    console.log("This object is already mapped");
                     return mappedObject;
                 }
                 else {
                     identityMap[className][object.model]= object;
                 }
             }
-            else{
+            else {
                 identityMap[className] = {};
                 identityMap[className][object.model] = object;
                 mappedObject = object;
             }
             return mappedObject;
         }
+        else {
+            console.log('This object is invalid');
+        }
     }
 
-    get(className,object)
-    {
-        let mappedObject; //null object
-        if (object){
-            if (identityMap[className]){
+    /**
+     * Returns an object in the Identity Map upon request
+     * @method get
+     * @param {Object} object an object of type electronicDevice
+     */
+    get(object) {
+        let className = object.constructor.name;
+        let mappedObject;
+        if (object) {
+            if (identityMap[className]) {
                 if (identityMap[className][object.model])
                 mappedObject = identityMap[className][object.model];
-                else{
+                else {
                     console.log('Error model number not found for that product');
                 }
             }
-            else
-            {
+            else {
                 console.log('This device class does not exist in the identity map');
             }
         }
         return mappedObject;
     }
 
-    delete(className, object) { 
+    /**
+     * Deletes an object in the Identity Map upon request
+     * @method delete
+     * @param {Object} object an object of type electronicDevice
+     */
+    delete(object) { 
+        var className = object.constructor.name;
         if(object) {
         delete identityMap[className][object.model];
         }
-        else    {
-            console.log('Invalid item delete');
+        else {
+            console.log('Invalid item');
         }
     }
 
+    /**
+     * Deletes all objects in the Identity Map upon logging out
+     * @method delete
+     */
     clear() {
         if (req.user) {
             console.log("Identity Map cannot be cleared, user is still logged in!");
