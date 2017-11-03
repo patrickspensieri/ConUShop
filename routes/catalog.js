b0c9bbece03f207d92bf66344c48c41715346474
tusmanakhter <tusman96@hotmail.com>
2017-11-02 20:03:07 -0400

    Cleaned up client routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,10 +3,10 @@ let router = new express.Router();
 let catalogController = require('../presentation-layer/controllers/catalogController');
 
 // Client routes
-router.get('/ClientPage', catalogController.clientPage);
-router.get('/ClientPage/Desktop', catalogController.clientDesktopView);
-router.get('/ClientPage/Laptop', catalogController.clientLaptopView);
-router.get('/ClientPage/Monitor', catalogController.clientMonitorView);
-router.get('/ClientPage/Tablet', catalogController.clientTabletView);
+router.get('/', catalogController.clientPage);
+router.get('/desktops', catalogController.clientDesktopView);
+router.get('/laptops', catalogController.clientLaptopView);
+router.get('/monitors', catalogController.clientMonitorView);
+router.get('/tablets', catalogController.clientTabletView);
 
 module.exports = router;
