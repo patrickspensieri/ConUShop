89d62cf1f112e5793d8fdbcff425027ce0ec5d77
tusmanakhter <tusman96@hotmail.com>
2017-11-03 20:57:20 -0400

    Added name and user type to the sign out modal

--- presentation-layer/controllers/accountController.js
+++ presentation-layer/controllers/accountController.js
@@ -95,6 +95,14 @@ module.exports = {
 
     ensureLoggedIn: function(req, res, next) {
         res.locals.isAuthenticated = req.isAuthenticated();
+        if (req.isAuthenticated()){
+            UserMapper.find(req.user.email, function(err, user) {
+                if (err) throw err;
+                if (user.isAdmin)
+                    res.locals.isAdmin = true;
+                res.locals.name = user.firstName + " " + user.lastName;
+            });
+        }
         return next();
     }
 };
